d8c9e047a2284cd6e9a3c12774e93fcb368d809a
Amit Kumar4 <Amit_Kumar@axa-in.intraxa>
2022-03-01 10:52:07 +0530

    updated1

--- Personal-Portfolio/Portfolio/Portfolio/urls.py
+++ Personal-Portfolio/Portfolio/Portfolio/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.static import serve
-from django.conf import settings
 
 
 urlpatterns = [
